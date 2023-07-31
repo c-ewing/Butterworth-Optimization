65dc6607b234beb09826ed4cad9278cc7a2a8ef6
Cameron Ewing <59128151+c-ewing@users.noreply.github.com>
2023-07-30 20:56:22 -0700

    butterworth.c: - Fixed point rewrite completed - All fixed point operations are now in fixedpoint.h rather than   cluttering up the butterworth.c file
    fixedpoint.h:
    - Customizable fixed point precision, currently set to Q17.15
    - Implement all required fixed point operations for butterworth.c:
      - add, sub, mul, div
      - str (conversion to decimal string)
      - from_real (conversion from float to fixed point, used for constants)
      - from_int (conversion from int to fixed point)
      - to_int (conversion from fixed point to int, truncating)
      - fractional_part (masks out the integer part of a fixed point number)
    - Define constants:
      - ZERO, ONE, TWO, HALF, PI, SQRT2

--- butterworth.c
+++ butterworth.c
@@ -3,11 +3,7 @@ REFERENCES:
 [1]     https://tttapa.github.io/Pages/Mathematics/Systems-and-Control-Theory/Digital-filters/Discretization/Discretization-of-a-fourth-order-Butterworth-filter.html
 [2]     https://en.wikipedia.org/wiki/Butterworth_filter
 [3,4]   https://www.youtube.com/watch?v=HJ-C4Incgpw AND https://github.com/curiores/ArduinoTutorials/blob/main/BasicFilters/Design/LowPass/ButterworthFilter.ipynb
-*/
-
-/*
-FLOATING POINT FORMAT:
-- 1 sign bit, 16 integer bits, 15 fractional bits
+[5]     https://sourceforge.net/projects/fixedptc/
 */
 
 #include <stdio.h>
@@ -15,198 +11,89 @@ FLOATING POINT FORMAT:
 #include <stdlib.h>
 #include <math.h>
 
-// Define M_PI if not already defined
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
-
-// Define Fixed point pi:
-// 1 sign bits, 2 integer bits, 29 fractional bits.
-// NOTE: probably way more precision than needed
-#define F_PI 1686629713 // 0b0_11_00100100001111110110101010001: 0 for positive, 3 integer, 0.14159265358979323846 fractional
+#include "fixedpoint.h"
 
 // Constants for Butterworth filter
 #define ORDER 2
 #define SAMPLING_RATE 22 * 1000   // Hertz [0, 65535]
 #define CUTOFF_FREQUENCY 2 * 1000 // Hertz [0, 65535], must be less than half the sampling rate
 
-// ## FIXED POINT FUNCTIONS ##
-typedef struct FixedPoint
-{
-    uint32_t value;
-} FixedPoint;
-
-// Get FixedPoint sign:
-uint32_t fixedPointGetSign(FixedPoint fp)
-{
-    return fp.value >> 31;
-}
-
-// Set FixedPoint sign:
-FixedPoint fixedPointSetSign(FixedPoint fp, uint32_t sign)
-{
-    fp.value = (fp.value & 0x7FFFFFFF) | (sign << 31);
-    return fp;
-}
-
-// Get FixedPoint integer:
-uint32_t fixedPointGetInteger(FixedPoint fp)
-{
-    return (fp.value >> 15) & 0x0000FFFF;
-}
-
-// Set FixedPoint integer:
-FixedPoint fixedPointSetInteger(FixedPoint fp, uint32_t integer)
-{
-    fp.value = (fp.value & 0x80007FFF) | (integer << 15);
-    return fp;
-}
-
-// Get FixedPoint fractional:
-uint32_t fixedPointGetFractional(FixedPoint fp)
-{
-    return fp.value & 0x00007FFF;
-}
-
-// Set FixedPoint fractional:
-FixedPoint fixedPointSetFractional(FixedPoint fp, uint32_t fractional)
-{
-    fp.value = (fp.value & 0xFFFF8000) | fractional;
-    return fp;
-}
-
-// Initialize a FixedPoint:
-FixedPoint fixedPointInit(uint32_t sign, uint32_t integer, uint32_t fractional)
-{
-    FixedPoint fp;
-    fp = fixedPointSetSign(fp, sign);
-    fp = fixedPointSetInteger(fp, integer);
-    fp = fixedPointSetFractional(fp, fractional);
-    return fp;
-}
-
-// Print FixedPoint value:
-void fixedPointPrint(FixedPoint fp)
-{
-    printf("%d:%d:%d (SIF)\n", fixedPointGetSign(fp), fixedPointGetInteger(fp), fixedPointGetFractional(fp));
-}
-
-// uint16_t to FixedPoint:
-FixedPoint uint16ToFixedPoint(uint16_t value)
-{
-    FixedPoint fp;
-    fp = fixedPointSetSign(fp, 0);
-    fp = fixedPointSetInteger(fp, value);
-    fp = fixedPointSetFractional(fp, 0);
-    return fp;
-}
-
-// FixedPoint to output uint16_t:
-// This rounds the value to the nearest integer and is on the range [-32767, 32767]
-// 1 sign, 15 integer
-uint16_t fixedPointToUint16(FixedPoint fp)
-{
-    // Get the sign
-    uint32_t sign = fixedPointGetSign(fp);
-
-    // Get the integer
-    uint32_t integer = fixedPointGetInteger(fp);
-
-    // Get the fractional
-    uint32_t fractional = fixedPointGetFractional(fp);
-
-    // Round the fractional
-    if (fractional >= 0x00004000)
-    {
-        integer += 1;
-    }
-
-    // TODO: scale the output down to 15 bits, right shift one and then round again
-    if (integer & 0x00000001)
-    {
-        integer = integer >> 1;
-        integer += 1;
-    }
-    else
-    {
-        integer = integer >> 1;
-    }
-
-    // Check for overflow
-    if (integer > 32767)
-    {
-        integer = 32767;
-    }
-
-    // Set the sign
-    if (sign)
-    {
-        integer = integer | 0x00008000;
-    }
-
-    return integer;
-}
-
 // Structure to hold Butterworth filter coefficients
 typedef struct FilterCoefficients
 {
     // Numerator coefficients
-    FixedPoint b0, b1, b2;
+    fixedpoint_t b0, b1, b2;
     // Denominator coefficients
-    FixedPoint a0, a1, a2;
+    fixedpoint_t a0, a1, a2;
 
     // Previous input
-    FixedPoint x1, x2;
+    fixedpoint_t x1, x2;
 
     // Previous output
-    FixedPoint y1, y2;
+    fixedpoint_t y1, y2;
 } ButterworthFilter;
 
 // Function to initialize Butterworth filter
-// TODO: Convert to fixed point
 void butterworthFilterInit(ButterworthFilter *filter)
 {
     // TODO: Switch to a lookup table rather than a calculated value for the specific case
     // double lambda = 1.0 / tan(M_PI * CUTOFF_FREQUENCY / SAMPLING_RATE);
-    FixedPoint lambda = fixedPointInit(0, 3, 13293); // TODO: Hardcoded the value for now
-    printf("lambda: ");
-    fixedPointPrint(lambda);
+    fixedpoint_t lambda = fixedpoint_from_real(3.40568723888925); // TODO: Hardcoded the value for now
+
+#ifdef DEBUG
+    printf("lambda:\t%s\n", fixedpoint_str(lambda));
+#endif
 
     // Calculate the coefficients
     // All of these are adjusted so that a0 is normalized to 1.0, a0 is calculated first and then used to scale all the other coefficients.
     // These coefficients are usually divided by a0 but thats slow so instead 1/a0 is calculated and multiplied.
-    // TODO: Optimization: pow(lambda, 2) is reused, might be good to try pulling that out into a variable
-    // TODO: Optimization: sqrt(2.0) * lambda is reused, might be good to try pulling that out into a variable
     // double inv_a0 = 1.0 / (pow(lambda, 2) + sqrt(2.0) * lambda + 1.0);
+    fixedpoint_t lambda_squared = fixedpoint_mul(lambda, lambda);
+    fixedpoint_t sqrt2_lambda = fixedpoint_mul(FIXEDPOINT_SQRT2, lambda);
+    fixedpoint_t a0 = lambda_squared + sqrt2_lambda + FIXEDPOINT_ONE;
+    fixedpoint_t inv_a0 = fixedpoint_div(FIXEDPOINT_ONE, a0);
+
+#ifdef DEBUG
+    printf("inv_a0:\t%s\n", fixedpoint_str(inv_a0));
+#endif
 
     // Calculate the coefficients:
-    // filter->a0 = 1.0; // Due to the normalizing around a0, this is always 1.0
-    // filter->a1 = (-2.0 * pow(lambda, 2) + 2.0) * inv_a0;
-    // filter->a2 = (pow(lambda, 2) - sqrt(2.0) * lambda + 1.0) * inv_a0;
+    filter->a0 = FIXEDPOINT_ONE; // Due to the normalizing around a0, this is always 1.0
+    // a1 = (-2.0 * pow(lambda, 2) + 2.0) * inv_a0;
+    filter->a1 = fixedpoint_mul((fixedpoint_mul(fixedpoint_from_int(-2), lambda_squared) + FIXEDPOINT_TWO), inv_a0);
+    // a2 = (pow(lambda, 2) - sqrt(2.0) * lambda + 1.0) * inv_a0;
+    filter->a2 = fixedpoint_mul((lambda_squared - sqrt2_lambda + FIXEDPOINT_ONE), inv_a0);
 
     // These are fixed for a second order Butterworth filter using the bilinear transform and adjusted using the value of a0.
-    // TODO: Optimization: b0 == b1
-    // filter->b0 = inv_a0;
-    // filter->b1 = 2.0 * inv_a0;
-    // filter->b2 = inv_a0;
+    filter->b0 = inv_a0;
+    // b1 = 2.0 * inv_a0
+    filter->b1 = fixedpoint_mul(FIXEDPOINT_TWO, inv_a0);
+    filter->b2 = inv_a0;
 
-    // Print the coefficients
-    // printf("b0: %d, b1: %d, b2: %d\n", filter->b0, filter->b1, filter->b2);
-    // printf("a0: %d, a1: %d, a2: %d\n", filter->a0, filter->a1, filter->a2);
+// Print the coefficients
+#ifdef DEBUG
+    printf("b0:\t%s\n", fixedpoint_str(filter->b0));
+    printf("b1:\t%s\n", fixedpoint_str(filter->b1));
+    printf("b2:\t%s\n", fixedpoint_str(filter->b2));
+
+    printf("a0:\t%s\n", fixedpoint_str(filter->a0));
+    printf("a1:\t%s\n", fixedpoint_str(filter->a1));
+    printf("a2:\t%s\n", fixedpoint_str(filter->a2));
+#endif
 
     // Initialize the previous input and output values to zero
-    filter->x1 = fixedPointInit(0, 0, 0);
-    filter->x2 = fixedPointInit(0, 0, 0);
-    filter->y1 = fixedPointInit(0, 0, 0);
-    filter->y2 = fixedPointInit(0, 0, 0);
+    filter->x1 = 0;
+    filter->x2 = 0;
+    filter->y1 = 0;
+    filter->y2 = 0;
 }
 
-// Function to apply Butterworth filter
-// TODO: Convert to fixed point
-FixedPoint butterworthFilterApply(ButterworthFilter *f, FixedPoint input)
+// Function to apply Butterworth filter to a single input
+fixedpoint_t butterworthFilterApply(ButterworthFilter *f, fixedpoint_t input)
 {
     // Calculate the output
-    FixedPoint output = (f->b0 * input + f->b1 * f->x1 + f->b2 * f->x2) - (f->a1 * f->y1 + f->a2 * f->y2);
+    // output = (f->b0 * input + f->b1 * f->x1 + f->b2 * f->x2) - (f->a1 * f->y1 + f->a2 * f->y2);
+    fixedpoint_t output = (fixedpoint_mul(f->b0, input) + fixedpoint_mul(f->b1, f->x1) + fixedpoint_mul(f->b2, f->x2)) - (fixedpoint_mul(f->a1, f->y1) + fixedpoint_mul(f->a2, f->y2));
 
     // Update the previous input and output values
     f->x2 = f->x1;
@@ -218,10 +105,23 @@ FixedPoint butterworthFilterApply(ButterworthFilter *f, FixedPoint input)
     return output;
 }
 
+uint16_t fixedpoint_to_uint16(fixedpoint_t input)
+{
+    // Adjust the range from [0,65535] of the input to [-32727, 32727] of the output
+    fixedpoint_t adjusted = fixedpoint_div(input, FIXEDPOINT_TWO);
+    fixedpoint_t scaled = adjusted + fixedpoint_from_int(32767);
+
+#ifdef DEBUG
+    printf("fp val:\t%s\n", fixedpoint_str(input));
+    printf("adj:\t%s\n", fixedpoint_str(adjusted));
+    printf("uint16:\t%s\n", fixedpoint_str(scaled));
+#endif
+    return fixedpoint_to_int(scaled);
+}
+
 int main(int argc, char *argv[])
 {
-    printf("Starting Butterworth filter\n");
-    printf("F_PI: %x\n", F_PI);
+    printf("Applying Butterworth Filter\n");
     if (argc < 3)
     {
         printf("Usage: %s <input_file> <output_file>\n", argv[0]);
@@ -245,19 +145,20 @@ int main(int argc, char *argv[])
     {
         if (c == '\n')
         {
-            ++numSamples;
+            numSamples++;
         }
     }
+    numSamples++; // Add one for the last line
+    // Reset the file pointer to the beginning of the file
     fseek(inputFile, 0, SEEK_SET);
 
     // Allocate memory for input and output buffers
-    // TODO: Change these to fixed point when the filter is working
-    FixedPoint *inputBuffer = (FixedPoint *)malloc(numSamples * sizeof(FixedPoint));
-    FixedPoint *outputBuffer = (FixedPoint *)malloc(numSamples * sizeof(FixedPoint));
+    fixedpoint_t *inputBuffer = (fixedpoint_t *)malloc(numSamples * sizeof(fixedpoint_t));
+    fixedpoint_t *outputBuffer = (fixedpoint_t *)malloc(numSamples * sizeof(fixedpoint_t));
 
     // Read input samples from file
     uint16_t sample;
-    for (size_t i = 0; i < numSamples; ++i)
+    for (size_t i = 0; i < numSamples; i++)
     {
         if (fscanf(inputFile, "%hu", &sample) != 1)
         {
@@ -266,28 +167,31 @@ int main(int argc, char *argv[])
         }
         else
         {
-            inputBuffer[i] = uint16ToFixedPoint(sample);
+            inputBuffer[i] = fixedpoint_from_int(sample);
         }
     }
 
     // Initialize the filter
     ButterworthFilter ButterworthFilter;
-
-    // TODO: NEXT CONVERSION
-    // Initialize the filter by calculating the coefficients
     butterworthFilterInit(&ButterworthFilter);
-    exit(0);
+
     // Apply Butterworth filter
-    // for (size_t i = 0; i < numSamples; ++i)
-    // {
-    //     outputBuffer[i] = butterworthFilterApply(&ButterworthFilter, inputBuffer[i]);
-    // }
+    for (size_t i = 0; i < numSamples; i++)
+    {
+        outputBuffer[i] = butterworthFilterApply(&ButterworthFilter, inputBuffer[i]);
+#ifdef DEBUG
+        printf("Input:\t%s\n", fixedpoint_str(inputBuffer[i]));
+        printf("Output:\t%s\n", fixedpoint_str(outputBuffer[i]));
+#endif
+    }
 
     // Write output samples to file
     for (size_t i = 0; i < numSamples; ++i)
     {
-        fprintf(outputFile, "%hu\n", fixedPointToUint16(outputBuffer[i]));
+        fprintf(outputFile, "%hu\n", fixedpoint_to_uint16(outputBuffer[i]));
     }
+
+    printf("Finished Applying Butterworth Filter\n");
 
     // Cleanup
     fclose(inputFile);
