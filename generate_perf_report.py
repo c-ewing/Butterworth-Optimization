1149108b5694dee6bee2642e687e35f887663ee7
Cameron Ewing <59128151+c-ewing@users.noreply.github.com>
2023-08-06 02:10:57 -0700

    Support skipping sections of the performance report skip

--- generate_perf_report.py
+++ generate_perf_report.py
@@ -1,4 +1,5 @@
 import subprocess
+import argparse
 
 # This script will generate a performance report for the Butterworth filter.
 # It will compile the program with different optimization flags and benchmark it using hyperfine.
@@ -17,8 +18,8 @@ OPTIMIZATION_FLAGS = ["O0", "O1", "O2", "O3", "Os", "Ofast"]
 
 # Hyperfine Configuration
 HYPERFINE = "hyperfine"
-HYPERFINE_RUNS = "15"
-HYPERFINE_FLAGS = ["--warmup", "3", "--runs",
+HYPERFINE_RUNS = "20"
+HYPERFINE_FLAGS = ["--warmup", "5", "--runs",
                    HYPERFINE_RUNS, "-L", "flag", "O0,O1,O2,O3,Os,Ofast", "--export-markdown", f"{TARGET}{EXECUTABLE_NAME}_perf.md"]
 HYPERFINE_COMMAND = [
     f"./{TARGET}{EXECUTABLE_NAME}_{{flag}} ts_sine.dat {TARGET}removeme_{{flag}}.dat"]
@@ -35,56 +36,74 @@ CALLGRIND_ANNOTATE_FLAGS = ["--auto=yes",
 
 # Test signal configuration
 SIGNAL_GEN = "python3"
+# Low number is used to reduce benchmarking time on the VM
+SIGNAL_NUM_SAMPLES = "11000"
 SIGNAL_FLAGS = ["testing/generate_test_signals.py", "--sample-rate",
                 "22000", "--num-samples", "44000"]
 
+# Parse command line arguments
+parser = argparse.ArgumentParser(
+    description="Generate a performance report for the Butterworth filter.")
+parser.add_argument("--skip-signal-gen", action="store_true",
+                    default=False, help="Skip generating the test signals.")
+parser.add_argument("--skip-hyperfine", action="store_true",
+                    default=False, help="Skip running hyperfine.")
+parser.add_argument("--skip-callgrind", action="store_true",
+                    default=False, help="Skip running callgrind.")
+
+args = parser.parse_args()
+
+
 # First we need to generate the test signals.
-print("Generating test signals")
-subprocess.run([SIGNAL_GEN, *SIGNAL_FLAGS])
+if not args.skip_signal_gen:
+    print("Generating test signals")
+    subprocess.run([SIGNAL_GEN, *SIGNAL_FLAGS])
 
 # Second we need to compile the program with different optimization flags and benchmark it using hyperfine.
 # Hyperfine will generate a markdown file with the results.
-for flag in OPTIMIZATION_FLAGS:
-    # Compile the program with the current optimization flag using subprocess
-    print(f"Compiling {TARGET} with {flag}")
-    subprocess.run([COMPILER, *COMPILER_FLAGS,
-                   f"-{flag}", "-o", f"{TARGET}{EXECUTABLE_NAME}_{flag}", f"{TARGET}{EXECUTABLE_NAME}.c"])
+if not args.skip_hyperfine:
+    for flag in OPTIMIZATION_FLAGS:
+        # Compile the program with the current optimization flag using subprocess
+        print(f"Compiling {TARGET} with {flag}")
+        subprocess.run([COMPILER, *COMPILER_FLAGS,
+                        f"-{flag}", "-o", f"{TARGET}{EXECUTABLE_NAME}_{flag}", f"{TARGET}{EXECUTABLE_NAME}.c"])
 
-# Run the benchmark using hyperfine
-print(f"Benchmarking target: {TARGET}")
-subprocess.run([HYPERFINE, *HYPERFINE_FLAGS, *HYPERFINE_COMMAND])
+    # Run the benchmark using hyperfine
+    print(f"Benchmarking target: {TARGET}")
+    subprocess.run([HYPERFINE, *HYPERFINE_FLAGS, *HYPERFINE_COMMAND])
 
-# Clean up the executables and signal data
-print(f"Cleaning benchmarking step for {TARGET}\n")
-for flag in OPTIMIZATION_FLAGS:
-    subprocess.run(["rm", f"{TARGET}removeme_{flag}.dat"])
-    subprocess.run(["rm", f"{TARGET}{EXECUTABLE_NAME}_{flag}"])
+    # Clean up the executables and signal data
+    print(f"Cleaning benchmarking step for {TARGET}\n")
+    for flag in OPTIMIZATION_FLAGS:
+        subprocess.run(["rm", f"{TARGET}removeme_{flag}.dat"])
+        subprocess.run(["rm", f"{TARGET}{EXECUTABLE_NAME}_{flag}"])
 
 # Finally use Callgrind to find instructions executed and the largest contributor to execution time.
 # This will generate a callgrind.out file that can be viewed using kcachegrind.
-print(f"Running Callgrind on target: {TARGET}")
-for flag in OPTIMIZATION_FLAGS:
-    # Compile the program with the current optimization flag and debug symbols using subprocess
-    # print(f"Compiling DEBUG {TARGET} with {flag}")
-    subprocess.run([COMPILER, *COMPILER_FLAGS, *COMPILER_DEBUG_SYMBOLS,
-                   f"-{flag}", "-o", f"{TARGET}{EXECUTABLE_NAME}_{flag}", f"{TARGET}{EXECUTABLE_NAME}.c"])
+if not args.skip_callgrind:
+    print(f"Running Callgrind on target: {TARGET}")
+    for flag in OPTIMIZATION_FLAGS:
+        # Compile the program with the current optimization flag and debug symbols using subprocess
+        # print(f"Compiling DEBUG {TARGET} with {flag}")
+        subprocess.run([COMPILER, *COMPILER_FLAGS, *COMPILER_DEBUG_SYMBOLS,
+                        f"-{flag}", "-o", f"{TARGET}{EXECUTABLE_NAME}_{flag}", f"{TARGET}{EXECUTABLE_NAME}.c"])
 
-    # Next we need to run the program with callgrind and generate the callgrind.out file
-    print(f"Callgrind on: {TARGET} with {flag}")
-    subprocess.run([CALLGRIND, *CALLGRIND_FLAGS, f"--callgrind-out-file={TARGET}{EXECUTABLE_NAME}_{flag}.out",
-                   f"./{TARGET}{EXECUTABLE_NAME}_{flag}", "ts_sine.dat", f"{TARGET}removeme_{flag}.dat"], stdout=subprocess.DEVNULL)
+        # Next we need to run the program with callgrind and generate the callgrind.out file
+        print(f"Callgrind on: {TARGET} with {flag}")
+        subprocess.run([CALLGRIND, *CALLGRIND_FLAGS, f"--callgrind-out-file={TARGET}{EXECUTABLE_NAME}_{flag}.out",
+                        f"./{TARGET}{EXECUTABLE_NAME}_{flag}", "ts_sine.dat", f"{TARGET}removeme_{flag}.dat"], stdout=subprocess.DEVNULL)
 
-    # Finally we need to run callgrind_annotate to generate a report
-    # print(f"Callgrind Annotate {TARGET} for {flag}")
-    # Open a file to redirect the output of callgrind_annotate into. Same as using ">" in bash.
-    with open(f"{TARGET}{EXECUTABLE_NAME}_{flag}.txt", "w") as f:
-        subprocess.run([CALLGRIND_ANNOTATE, *CALLGRIND_ANNOTATE_FLAGS,
-                       f"{TARGET}{EXECUTABLE_NAME}_{flag}.out"], stdout=f)
+        # Finally we need to run callgrind_annotate to generate a report
+        # print(f"Callgrind Annotate {TARGET} for {flag}")
+        # Open a file to redirect the output of callgrind_annotate into. Same as using ">" in bash.
+        with open(f"{TARGET}{EXECUTABLE_NAME}_{flag}.txt", "w") as f:
+            subprocess.run([CALLGRIND_ANNOTATE, *CALLGRIND_ANNOTATE_FLAGS,
+                            f"{TARGET}{EXECUTABLE_NAME}_{flag}.out"], stdout=f)
 
-    # Clean up the executables
-    print(f"Cleaning callgrind step for {TARGET} with flag {flag}\n")
-    subprocess.run(["rm", f"{TARGET}removeme_{flag}.dat",
-                   f"{TARGET}{EXECUTABLE_NAME}_{flag}"])
+        # Clean up the executables
+        print(f"Cleaning callgrind step for {TARGET} with flag {flag}\n")
+        subprocess.run(["rm", f"{TARGET}removeme_{flag}.dat",
+                        f"{TARGET}{EXECUTABLE_NAME}_{flag}"])
 
 # Clean up the signal data
 print(f"Cleaning signal data for target: {TARGET}\n")
