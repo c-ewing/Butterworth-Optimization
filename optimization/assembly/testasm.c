40551b49e8a5d8d4a6228fa3d2448a5fcc5500e5
Cameron Ewing <59128151+c-ewing@users.noreply.github.com>
2023-08-09 12:59:54 -0700

    Prototype assembly multiply

--- optimization/assembly/testasm.c
+++ optimization/assembly/testasm.c
@@ -1,24 +1,25 @@
 #include <stdint.h>
 #include <stdio.h>
+#include "fixedpoint.h"
 
 int main(int argc, char const *argv[])
 {
-    int32_t a = 2 << 15;
-    int32_t b = 3 << 15;
+    fixedpoint_t a = 2 << 15;
+    fixedpoint_t b = 3 << 15;
 
-    int64_t c = (a * b) >> 15;
-    int64_t result = 0;
-    printf("c: %lld\n", c);
+    int32_t result_l;
+    int32_t result_h;
+    printf("c: %s\n", fixedpoint_str(fixedpoint_mul(a, b)));
 
-    asm(
-        "smull   %[result_lo], %[result_hi], %[a], %[b]   \n"           // Multiply a and b, store result in result_lo and result_hi
-        "asr     %[result_hi], %[result_hi], #15         \n"            // Shift result_hi right by 15 bits
-        "orr     %[result_lo], %[result_lo], %[result_hi], lsl #17  \n" // Combine result_lo and shifted result_hi
-        : [result_lo] "=r"(result), [result_hi] "+r"(result)
-        : [a] "r"(a), [b] "r"(b)
-        : "cc");
-
-    printf("result: %lld\n", result);
+    // asm(
+    //     "smull   %[result_lo], %[result_hi], %[a], %[b]   \n\t"           // Multiply a and b, store result in result_lo and result_hi
+    //     "asr     %[result_hi], %[result_hi], #15         \n\t"            // Shift result_hi right by 15 bits
+    //     "orr     %[result_lo], %[result_lo], %[result_hi], lsl #17  \n\t" // Combine result_lo and shifted result_hi
+    //     : [result_lo] "=r"(result_l), [result_hi] "+r"(result_h)
+    //     : [a] "r"(a), [b] "r"(b)
+    //     : "cc");
+    // int64_t result = ((int64_t)result_h << 32) | (result_l & 0xffffffff);
+    // printf("result: %s\n", result);
 
     return 0;
 }
