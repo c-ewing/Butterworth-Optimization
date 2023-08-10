a51e05c40d6d0888ae013a2c0b6495a85f365b84
Cameron Ewing <59128151+c-ewing@users.noreply.github.com>
2023-08-09 20:56:15 -0700

    Fix

--- optimization/loop_unroll_4/butterworth.c
+++ optimization/loop_unroll_4/butterworth.c
@@ -139,7 +139,7 @@ int main(int argc, char *argv[])
 
     // Count the number of lines in the input file
     // TODO: Optimization: dont scan the file, instead apply as reading the file
-    uint32_t numSamples = 0;
+    size_t numSamples = 0;
     int c;
     while ((c = fgetc(inputFile)) != EOF)
     {
@@ -158,7 +158,7 @@ int main(int argc, char *argv[])
 
     // Read input samples from file
     uint16_t sample;
-    for (uint32_t i = 0; i < numSamples; i++)
+    for (size_t i = 0; i < numSamples; i++)
     {
         if (fscanf(inputFile, "%hu", &sample) != 1)
         {
@@ -176,7 +176,7 @@ int main(int argc, char *argv[])
     butterworthFilterInit(&ButterworthFilter);
 
     // Apply Butterworth filter, unrolling the loop for performance
-    for (uint32_t i = 0; i < numSamples; i += 4)
+    for (size_t i = 0; i < numSamples; i += 4)
     {
         outputBuffer[i] = butterworthFilterApply(&ButterworthFilter, inputBuffer[i]);
         outputBuffer[i + 1] = butterworthFilterApply(&ButterworthFilter, inputBuffer[i + 1]);
@@ -185,13 +185,13 @@ int main(int argc, char *argv[])
     }
 
     // Apply Butterworth filter to the last samples if there is an odd number of samples
-    for (uint32_t i = numSamples - (numSamples % 4); i < numSamples; i++)
+    for (size_t i = numSamples - (numSamples % 4); i < numSamples; i++)
     {
         outputBuffer[i] = butterworthFilterApply(&ButterworthFilter, inputBuffer[i]);
     }
 
     // Write output samples to file
-    for (uint32_t i = 0; i < numSamples; ++i)
+    for (size_t i = 0; i < numSamples; ++i)
     {
         fprintf(outputFile, "%hu\n", fixedpoint_to_uint16(outputBuffer[i]));
     }
